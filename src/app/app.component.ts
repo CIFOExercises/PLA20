4005f72e0bea07c7c90c4a49b3b61ec0bb59b4d3
jmhs11 <josemariahs@gmail.com>
2021-07-15 14:46:49 +0200

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { TodoService } from './services/todo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'PLA20';
+
+  constructor(private service: TodoService) {}
+
+  public getAllTodos(): void {
+    this.service.getAllTodos().subscribe((todos) => console.log(todos));
+  }
+
+  public getTodo(): void {
+    this.service.getTodo('1').subscribe((todo) => console.log(todo));
+  }
 }
---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Todo } from '../models/todo';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoService {
+  private basePath = 'https://jsonplaceholder.typicode.com';
+  constructor(private httpClient: HttpClient) {}
+
+  getAllTodos(): Observable<Todo[]> {
+    return this.httpClient.get<Todo[]>(`${this.basePath}/todos`);
+  }
+
+  getTodo(id: string): Observable<Todo> {
+    return this.httpClient.get<Todo>(`${this.basePath}/todos/${id}`);
+  }
+
+  addTodo(todo: Todo): Observable<Todo> {
+    return this.httpClient.post<Todo>(`${this.basePath}/todos`, todo);
+  }
+
+  updateTodo(todo: Todo) {
+    return this.httpClient.put<Todo>(`${this.basePath}/todos`, todo);
+  }
+
+  deleteTodo(id: string) {}
+}
